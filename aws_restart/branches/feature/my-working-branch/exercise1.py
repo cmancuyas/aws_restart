4e8401b32fe337e0d187afeb0ba70aa12d853092
Christopher Mancuyas <yashiro_08@yahoo.com>
2023-11-12 00:28:28 +0800

    add creds

---
+++ aws_restart/branches/feature/my-working-branch/exercise1.py
@@ -0,0 +1,33 @@
+print("Python has three numeric types: int, float and complex")
+
+# myValue=True
+
+# print(myValue)
+
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# myString = "This is a string."
+# print(myString)
+
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + " " + secondString
+print(thirdString)
+
+name = input("What is your name? ")
+
+print(name)
+
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+
+print("{}, you like a {} {}!".format(name,color,animal))
+
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+
+print(myFruitList[1])
+print(myFruitList[2])
